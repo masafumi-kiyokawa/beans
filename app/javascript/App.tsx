aa1da52e85700ec3b41e8ce2e1ac023af28a0f20
fujitsubo <110240244+masafumi-kiyokawa@users.noreply.github.com>
2024-01-22 20:28:19 +0900

    Change home into beans

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="grid md:grid-cols-2">
       <div className="md:col-span-2 bg-blue-200">Content A</div>
 
-      <div className="flex flex-row">
-        <div className="md:col-span-1 md:w-52 bg-green-200 hidden md:block">
-          Content B
-        </div>
-        <div className="md:col-span-2 md:w-auto bg-yellow-200">Content C</div>
+      <div className="md:col-span-1 lg:w-200 bg-green-200 hidden md:block">
+        Content B
       </div>
+
+      <div className="md:col-span-1 lg:flex- bg-yellow-200">Content C</div>
     </div>
   );
 };
